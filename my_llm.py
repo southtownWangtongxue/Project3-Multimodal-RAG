ac3d55b80217ffa4b1dc78678031769d66141e47
165019 <mgn34379@qq.com>
2026-05-13 17:25:37 +0800

    nvidia-提供商

--- my_llm.py
+++ my_llm.py
@@ -1,9 +1,13 @@
 from langchain.chat_models import init_chat_model
+from langchain_core.rate_limiters import InMemoryRateLimiter
 
 from utils.env_utils import DASHSCOPE_API_KEY, ALIBABA_BASE_URL, ZHIPU_API_KEY, ZHIPU_BASE_URL, OPENAI_API_KEY, \
     OPENAI_BASE_URL
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=2/3,  # <-- rpm=40
+    check_every_n_seconds=0.1,  #每隔多少秒检查一次令牌是否可用
+)
 llm = init_chat_model(
     model='qwen3.5-flash-2026-02-23',
     model_provider='openai',
@@ -20,12 +24,14 @@ glm = init_chat_model(
     base_url=ZHIPU_BASE_URL
 )
 
-gpt = init_chat_model(
-    model='gpt-4o-mini',
-    model_provider='openai',
+nvidia = init_chat_model(
+    model='deepseek-ai/deepseek-v4-pro',
+    model_provider='nvidia',
     temperature=1.0,
     api_key=OPENAI_API_KEY,
-    base_url=OPENAI_BASE_URL
+    base_url=OPENAI_BASE_URL,
+    reasoning_effort='max',
+    rate_limiter=rate_limiter
 )
 bge_large = HuggingFaceEmbeddings(
     model_name='BAAI/bge-large-zh-v1.5',
