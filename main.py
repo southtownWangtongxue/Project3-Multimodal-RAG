f550e6c72b04e846c38c79cf7f4da986e95ae3c6
165019 <mgn34379@qq.com>
2026-05-14 17:03:41 +0800

    保存按钮实现

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import pathlib
 import gradio as gr
 
 from dots_mocr.parser import do_parse
+from milvus_db.db_operator import embedding_to_save
+from splitters.splitter_md import MarkdownDirSplitter
 from utils.common_utils import get_filename, delete_directory_if_non_empty, get_sorted_md_files
 from utils.log_utils import log
 
 OUTPUT_DIR = pathlib.Path(__file__).parent / "output"
-print(OUTPUT_DIR)
+log.info('切片程序主界面启动')
 
 
 class FileEmbeddingWebUI:
@@ -39,18 +41,18 @@ class FileEmbeddingWebUI:
 
     def parse_pdf(self):
         """解析pdf文件，变成多个md文件"""
-        #创建解析后输出目录OUTPUT_DIR+上传pdf文件名
+        # 创建解析后输出目录OUTPUT_DIR+上传pdf文件名
         parse_md_dir = os.path.join(OUTPUT_DIR, get_filename(self.pdf_path, False))
-        #创建前先删除同名目录
+        # 创建前先删除同名目录
         delete_directory_if_non_empty(parse_md_dir)
-        #开始解析
+        # 开始解析
         do_parse(input_path=self.pdf_path, num_thread=32, no_fitz_preprocess=True)
-        #解析完成后，保存类属性,默认展示第一个MarkDown，更新状态
+        # 解析完成后，保存类属性,默认展示第一个MarkDown，更新状态
         if os.path.isdir(parse_md_dir):
             self.md_dir = parse_md_dir
             log.info(f"PDF已解析，生成了{len(os.listdir(parse_md_dir))}个md文件")
             self.md_files = get_sorted_md_files(self.md_dir)
-            log.info(f"PDF已解析，生成的MD文件列表：{self.md_files}")
+            log.info(f"PDF已解析，生成的MD文件列表：{self.md_files},存放位置：{self.md_dir}")
             # 把第一个文件的内容展示出来
             # 读取所有的md文件内容
             for f in self.md_files:
@@ -75,8 +77,6 @@ class FileEmbeddingWebUI:
                 gr.update(interactive=False)  # save_btn - 使用 gr.update
             ]
 
-
-
     def select_md_file(self, selected_file):
         """选择一个md文件，并展示它的内容"""
         log.info(f"选择文件：{selected_file}")
@@ -92,6 +92,16 @@ class FileEmbeddingWebUI:
                 return "没有找到该文件"
         else:
             return "文件内容加载失败,选择的文件不对"
+
+    def embedding_save(self):
+        splitter = MarkdownDirSplitter(images_output_dir=str(OUTPUT_DIR / 'images'))
+        docs = splitter.process_md_dir(str(self.md_dir), source_filename=self.pdf_path)
+        res = embedding_to_save(docs)
+        for i, doc in enumerate(res):
+            print(f"\n文档 #{i + 1}:")
+            print(doc['text'], doc['image_path'])
+        return f"成功存入 {len(res)} 个文档到Milvus"
+
     def create_web_ui(self):
         """创建一个构建多模态知识库的界面"""
 
@@ -110,7 +120,7 @@ class FileEmbeddingWebUI:
                 # MD文件中的内容
                 content = gr.Textbox(label="文件内容", lines=20, interactive=False)
 
-            save_btn = gr.Button("存入知识库", variant="secondary", interactive=False)
+            save_btn = gr.Button("存入知识库", variant="huggingface", interactive=False)
 
             # 绑定按钮点击事件
             pdf_upload.change(
@@ -129,6 +139,11 @@ class FileEmbeddingWebUI:
                 fn=self.select_md_file,
                 inputs=file_dropdown,
                 outputs=content
+            )
+            save_btn.click(
+                fn=self.embedding_save,
+                inputs=[],
+                outputs=status
             )
 
         return app
